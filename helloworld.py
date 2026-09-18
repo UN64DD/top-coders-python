abcb36e592be238a2b2e64871a7ac0ad8d7d1e94
UN64DD <azhar.janius2005@gmail.com>
2026-09-18 23:41:00 +0800

    first commit

---
+++ helloworld.py
@@ -0,0 +1,11 @@
+print("Hello World")
+
+pi = 3.14159
+
+degrees = 180
+
+print("Degrees: "+ str(degrees))
+
+radians = (pi/180) * degrees
+
+print("Radians: "+ str(degrees))
---
+++ numbers.py
@@ -0,0 +1,6 @@
+age = 35
+pi = 3.14159
+
+math_operation = 1 + 3 * 4 / 2 - 2
+
+print(math_operation)
